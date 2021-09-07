7614c4712d19cd5ef8b799a2f70311a99adafde8
Opilane TTHK <opilane@tthk.ee>
2021-09-07 14:10:03 +0300

    Add ReadLine

---
+++ Greetings/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Greetings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //progamm küsib kajutaja nime
+            //progamm tervitab kasutajat nimepidi
+            Console.WriteLine("Mis su nimi on?");
+
+            string UserName = Console.ReadLine();
+
+            //Console.WriteLine("Hello, = +Username);
+
+            //string interpolation
+
+            // {}- altGr + 7 / altGr + 0
+            Console.WriteLine($"Hello, {UserName}");
+
+
+            Console.Read();
+        }
+    }
+}
---
+++ GreetingsV2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GreetingsV2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Programm küsib kajutaja eesnime
+            //Programm Küsib kajutaja perekonna nime
+            //Programm tervitab kasutajat, kasutades nid andmeid
+            Console.WriteLine("Mis on sinu ees nimi?");
+            string UserName = Console.ReadLine();
+            Console.WriteLine("Mis su perekonna nimi on?");
+            string LasName = Console.ReadLine();
+            Console.WriteLine($"Hello, {UserName} {LasName}");
+            Console.ReadLine();
+        }
+    }
+}
